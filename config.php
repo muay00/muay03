3a70afd52974db8b1f6fb363e4a4aff1df6b281a
muay00 <suchawadee233ta@gmail.com>
2024-12-17 16:39:50 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,20 +1,15 @@
 <?php
-
-
 $severname = "localhost";
 $username = "root";
 $userpass = "";
-$dbname = "form";
-
+$dbname = "muay03";
 try{
     $dbcon = new PDO("mysql:host=$severname;dbname=$dbname", $username,
     $userpass);
-
     $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //  echo "connect to database";
     
     }catch(PDOException $e) {
     echo"Failed to content to database" . $e->getMessage();
     }
-
 ?>
